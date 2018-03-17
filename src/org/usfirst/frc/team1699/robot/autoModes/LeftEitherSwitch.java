5168970fa7c703c7da024975f3e4b3c2e570ebd3
Administrator <Administrator@t420-fdzgk.mshome.net>
2018-03-17 09:25:22 -0400

    New Autos

---
+++ src/org/usfirst/frc/team1699/robot/autoModes/LeftEitherSwitch.java
@@ -0,0 +1,12 @@
+package org.usfirst.frc.team1699.robot.autoModes;
+
+import org.usfirst.frc.team1699.utils.autonomous.AutoMode;
+
+public class LeftEitherSwitch implements AutoMode{
+
+	@Override
+	public void runAuto() {
+		
+	}
+
+}
---
+++ src/org/usfirst/frc/team1699/robot/autoModes/RightEitherScale.java
@@ -0,0 +1,12 @@
+package org.usfirst.frc.team1699.robot.autoModes;
+
+import org.usfirst.frc.team1699.utils.autonomous.AutoMode;
+
+public class RightEitherScale implements AutoMode{
+
+	@Override
+	public void runAuto() {
+		
+	}
+
+}
